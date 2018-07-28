7bb8c3a2ddda0f905392fa14a572d98c32457ad2
victorloo <ila9970@gmail.com>
2018-07-29 03:07:38 +0800

    add AuthPanel

---
+++ src/components/AuthPanel.vue
@@ -0,0 +1,95 @@
+<template>
+<div class="auth-panel">
+  <h1>Please {{ content }}</h1>
+  <input type="email"
+    name="email"
+    id="email"
+    placeholder="Email Address"
+    v-model="email">
+  <input type="password"
+    name="password"
+    id="password"
+    placeholder="Password"
+    v-model="password">
+  <button @click="handleSubmit">{{ content }}</button>
+</div>
+</template>
+
+<script>
+export default {
+  props: ["type"],
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  computed: {
+    content: function() {
+      if (this.type === "login") {
+        return "Login";
+      } else {
+        return "Sign up";
+      }
+    }
+  },
+  methods: {
+    handleSubmit: function() {
+      const payload = {
+        email: this.email,
+        password: this.password
+      };
+      console.log("submit auth");
+      console.dir(payload);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.auth-panel {
+  width: 300px;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.auth-panel h1 {
+  width: 100%;
+}
+.input-container {
+  width: 100%;
+  border: 1px solid #c0c0c0;
+  border-radius: 6px;
+}
+input {
+  width: 100%;
+  height: 40px;
+  font-size: 16px;
+}
+#email {
+  border: 1px solid #c0c0c0;
+  border-radius: 6px 6px 0 0;
+}
+#email:focus {
+  outline: none;
+}
+#password {
+  border: 1px solid #c0c0c0;
+  border-radius: 0 0 6px 6px;
+  margin-bottom: 15px;
+}
+#password:focus {
+  outline: none;
+}
+button {
+  width: 100%;
+  height: 40px;
+  background-color: rgb(54, 107, 158);
+  border-radius: 6px;
+  border: none;
+  color: #fff;
+  font-size: 18px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import ImgDisplay from "@/components/ImgDisplay";
 import ItemListElement from "@/components/ItemListElement";
+import AuthPanel from "@/components/AuthPanel";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: "/ItemListElement",
       name: "ItemListElement",
       component: ItemListElement
+    },
+    {
+      path: "/AuthPanel",
+      name: "AuthPanel",
+      component: AuthPanel
     }
   ]
 });
