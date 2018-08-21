acf2e049886cff848fd4fa4b5d5f079f31fbfb73
victorloo <ila9970@gmail.com>
2018-08-21 19:47:48 +0800

    AuthPanel post user data to signup api

--- src/components/AuthPanel.vue
+++ src/components/AuthPanel.vue
@@ -39,6 +39,7 @@ export default {
         email: this.email,
         password: this.password
       };
+      this.$emit("auth-panel-submit", payload);
       console.log("submit auth");
       console.dir(payload);
     }
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,14 +1,32 @@
 <template>
 <div class="signup">
-  <AuthPanel type="signup" />
+  <AuthPanel type="signup" @auth-panel-submit="handleSignUp" />
 </div>
 </template>
 
 <script>
 import AuthPanel from "@/components/AuthPanel";
+import axios from "axios";
+
 export default {
   components: {
     AuthPanel: AuthPanel
+  },
+  methods: {
+    handleSignUp: function(payload) {
+      var that = this;
+      var signupUrl = "http://35.185.111.183/api/v1/signup";
+      axios
+        .post(signupUrl, payload)
+        .then(function(res) {
+          console.log("Signup Success");
+          console.log(res.data);
+          that.$router.push("/login");
+        })
+        .catch(function(err) {
+          console.error(err.response.data.errors);
+        });
+    }
   }
 };
 </script>
