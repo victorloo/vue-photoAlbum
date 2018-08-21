80b0ed311136f31bbc777f9313db065132114b41
victorloo <ila9970@gmail.com>
2018-08-21 20:47:26 +0800

    AuthPanel request login to api

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,40 @@
 <template>
 <div class="login">
-  <AuthPanel type="login" />
+  <AuthPanel type="login"  @auth-panel-submit="handleLogin" />
 </div>
 </template>
 
 <script>
 import AuthPanel from "@/components/AuthPanel";
+import axios from "axios";
 export default {
   components: {
     AuthPanel: AuthPanel
+  },
+  methods: {
+    handleLogin: function(payload) {
+      console.log("handleLogin");
+      var that = this;
+      var url = "http://35.185.111.183/api/v1/login";
+      axios
+        .post(url, payload)
+        .then(function(res) {
+          console.log("login success!");
+          console.log(res.data.auth_token);
+          //save to localstorage
+          var authToken = res.data.auth_token;
+          var email = payload.email;
+          var sessionData = { authToken: authToken, email: email };
+          localStorage.setItem("photo-album-user", JSON.stringify(sessionData));
+          //emit 'auth-state' to $bus
+          that.$bus.$emit("auth-state", { action: "login" });
+          //go to index
+          that.$router.push("/");
+        })
+        .catch(function(err) {
+          console.error(err.response.data.errors);
+        });
+    }
   }
 };
 </script>
