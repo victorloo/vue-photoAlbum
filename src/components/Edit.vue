58f18e49d33d3c698119e6768d56ae772e0e4834
victorloo <ila9970@gmail.com>
2018-08-21 20:59:16 +0800

    Edit page get photo data from show api

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,10 +1,19 @@
 <template>
 <div class="edit">
   <div class="photo-container">
-    <ImgDisplay/>
+    <!--pass url to ImgDisplay-->
+    <ImgDisplay 
+      :url="'http://35.185.111.183'+photo.file_location.url" 
+      v-if="photo.file_location" 
+    />
   </div>
   <div class="main-container">
-    <PhotoForm/>
+    <!--pass photo info to PhotoForm-->
+    <PhotoForm 
+      :title="photo.title"
+      :description="photo.description"
+      v-if="photo.title" 
+    />
   </div>
 </div>
 </template>
@@ -12,6 +21,7 @@
 <script>
 import ImgDisplay from "@/components/ImgDisplay";
 import PhotoForm from "@/components/PhotoForm";
+import axios from "axios";
 export default {
   components: {
     ImgDisplay: ImgDisplay,
@@ -21,6 +31,21 @@ export default {
     return {
       photo: {}
     };
+  },
+  created() {
+    var that = this;
+    var id = this.$route.params.id;
+    var url = "http://35.185.111.183/api/v1/photos/" + id;
+    var token = JSON.parse(localStorage.getItem("photo-album-user")).authToken;
+    var params = { auth_token: token };
+    axios
+      .get(url, { params })
+      .then(function(res) {
+        that.photo = res.data;
+      })
+      .catch(function(err) {
+        console.error(err.response.data);
+      });
   }
 };
 </script>
